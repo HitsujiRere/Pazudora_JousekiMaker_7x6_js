d6b13e700f14681ca7f5c263191c858391baf7a7
HitsujiRere <hitsujirere@gmail.com>
2020-10-07 19:54:11 +0900

    Add deploy mode.

--- Pazudora_JousekiMaker_7x6_js.js
+++ Pazudora_JousekiMaker_7x6_js.js
@@ -22,22 +22,35 @@ let dropIsPressed = false;
 let pressedDropY = 0;
 let pressedDropX = 0;
 
-// 選択しているボタン
+// ボタンの表示テキスト
 const buttonTexts = ['random', 'deploy', 'play']
-let selectedButton = 0;
+// 選択しているボタン
+let selectedButton = 2;
+
+// 配置するドロップ
+let deployDrop = 0;
+
+// ドロップを動かしたかどうか
+let dropIsMoved = false;
+
+// 消しているかどうか
+let isJudging = false;
+// 消すボード
+const boardErace = Array.from(new Array(boardHeight),
+    () => new Array(boardWidth).fill(0));
 
 function preload() {
 
     // ドロップの画像読込み
     for (let i = 0; i < dropCount; ++i) {
-        print('images/drop_' + i + '.png');
         dropImages[i] = loadImage('images/drop_' + i + '.png');
         //dropImages[i].resize(dropDisplaySize, dropDisplaySize);
     }
 }
 
 function setup() {
-    createCanvas(dropDisplaySize * boardWidth, dropDisplaySize * (boardHeight + 2));
+    createCanvas(dropDisplaySize * boardWidth,
+        dropDisplaySize * (boardHeight + 2));
 
     textAlign(CENTER, CENTER);
     imageMode(CENTER);
@@ -52,6 +65,21 @@ function draw() {
 }
 
 function update() {
+
+    if (isJudging) {
+
+        judgeBoard()
+    } else {
+
+        dropUpdate();
+
+        buttonUpdate();
+    }
+
+    pmouseIsPressed = mouseIsPressed;
+}
+
+function dropUpdate() {
     // ドロップ
     if (mouseIsPressed) {
 
@@ -70,35 +98,57 @@ function update() {
         }
 
         if (pressedDropYNow !== -1 && pressedDropYNow !== -1) {
-            if (dropIsPressed) {
-                // 交換する
-                let tmp = board[pressedDropY][pressedDropX];
-                board[pressedDropY][pressedDropX] = board[pressedDropYNow][pressedDropXNow];
-                board[pressedDropYNow][pressedDropXNow] = tmp;
 
-                pressedDropY = pressedDropYNow;
-                pressedDropX = pressedDropXNow;
-            } else {
-                dropIsPressed = true;
-                pressedDropY = pressedDropYNow;
-                pressedDropX = pressedDropXNow;
+            if (selectedButton === 1) {
+                board[pressedDropYNow][pressedDropXNow] = deployDrop;
+            }
+
+            if (selectedButton === 2) {
+                if (dropIsPressed && (pressedDropY !== pressedDropYNow || pressedDropX !== pressedDropXNow)) {
+                    // 交換する
+                    let tmp = board[pressedDropY][pressedDropX];
+                    board[pressedDropY][pressedDropX] =
+                        board[pressedDropYNow][pressedDropXNow];
+                    board[pressedDropYNow][pressedDropXNow] = tmp;
+
+                    pressedDropY = pressedDropYNow;
+                    pressedDropX = pressedDropXNow;
+
+                    dropIsMoved = true;
+                } else {
+                    dropIsPressed = true;
+                    pressedDropY = pressedDropYNow;
+                    pressedDropX = pressedDropXNow;
+                }
             }
         }
     } else {
+        if (dropIsPressed && dropIsMoved) {
+            if (selectedButton === 2) {
+                isJudging = true;
+            }
+
+            dropIsMoved = false;
+        }
+
         dropIsPressed = false;
         pressedDropY = -1;
         pressedDropX = -1;
     }
+}
 
-    // ボタン
+// ボタンの更新
+function buttonUpdate() {
     if (mouseIsPressed && !pmouseIsPressed) {
 
         // 今押しているボタン
         let pressedButtonNow = -1;
 
         for (let i = 0; i < 3; ++i) {
-            if (width / 3 * i <= mouseX && mouseX < width / 3 * (i + 1) &&
-                dropDisplaySize * boardHeight <= mouseY && mouseY < dropDisplaySize * (boardHeight + 1)) {
+            if (width / 3 * i <= mouseX &&
+                mouseX < width / 3 * (i + 1) &&
+                dropDisplaySize * boardHeight <= mouseY &&
+                mouseY < dropDisplaySize * (boardHeight + 1)) {
                 pressedButtonNow = i;
             }
         }
@@ -112,7 +162,30 @@ function update() {
         }
     }
 
-    pmouseIsPressed = mouseIsPressed;
+    deploySelectUpdate();
+}
+
+// 配置の選択ドロップのアップデート
+function deploySelectUpdate() {
+    // 配置
+    if (mouseIsPressed && selectedButton === 1) {
+
+        // 今押しているボタン
+        let pressedButtonNow = -1;
+
+        for (let i = 0; i < dropCount; ++i) {
+            if (dropDisplaySize * (i) <= mouseX &&
+                mouseX < dropDisplaySize * (i + 1) &&
+                dropDisplaySize * (boardHeight + 1) <= mouseY &&
+                mouseY < dropDisplaySize * (boardHeight + 2)) {
+                pressedButtonNow = i;
+            }
+        }
+
+        if (pressedButtonNow !== -1) {
+            deployDrop = pressedButtonNow;
+        }
+    }
 }
 
 function display() {
@@ -131,27 +204,6 @@ function display() {
             rect(dropDisplaySize * x, dropDisplaySize * y,
                 dropDisplaySize, dropDisplaySize);
         }
-    }
-
-    // ドロップ
-    for (let y = 0; y < boardHeight; ++y) {
-        for (let x = 0; x < boardWidth; ++x) {
-            if (y !== pressedDropY || x !== pressedDropX) {
-                image(dropImages[board[y][x]],
-                    dropDisplaySize * (x + 0.5), dropDisplaySize * (y + 0.5),
-                    dropDisplaySize, dropDisplaySize);
-            }
-
-            // fill(255, 0, 0, 200);
-            // ellipse(dropDisplaySize * (x + 0.5), dropDisplaySize * (y + 0.5), dropDisplaySize);
-        }
-    }
-
-    // 押しているドロップを拡大表示
-    if (dropIsPressed) {
-        image(dropImages[board[pressedDropY][pressedDropX]],
-            mouseX, mouseY,
-            dropDisplaySize * 1.3, dropDisplaySize * 1.3);
     }
 
     // ボタン
@@ -177,11 +229,35 @@ function display() {
         // 編集ドロップ
         if (i === selectedButton && i === 1) {
             for (let i = 0; i < dropCount; ++i) {
+                if (i !== deployDrop) {
+                    tint(128);
+                }
                 image(dropImages[i],
-                    dropDisplaySize * (i + 0.5), dropDisplaySize * (boardHeight + 1.5),
+                    dropDisplaySize * (i + 0.5),
+                    dropDisplaySize * (boardHeight + 1.5),
+                    dropDisplaySize, dropDisplaySize);
+                noTint();
+            }
+        }
+    }
+
+    // ドロップ
+    for (let y = 0; y < boardHeight; ++y) {
+        for (let x = 0; x < boardWidth; ++x) {
+            if (y !== pressedDropY || x !== pressedDropX) {
+                image(dropImages[board[y][x]],
+                    dropDisplaySize * (x + 0.5),
+                    dropDisplaySize * (y + 0.5),
                     dropDisplaySize, dropDisplaySize);
             }
         }
+    }
+
+    // 押しているドロップを拡大表示
+    if (dropIsPressed) {
+        image(dropImages[board[pressedDropY][pressedDropX]],
+            mouseX, mouseY,
+            dropDisplaySize * 1.3, dropDisplaySize * 1.3);
     }
 }
 
@@ -190,6 +266,17 @@ function randomizeBoard() {
     for (let y = 0; y < boardHeight; ++y) {
         for (let x = 0; x < boardWidth; ++x) {
             board[y][x] = Math.floor(Math.random() * dropCount);
+        }
+    }
+}
+
+// ボードを判定する
+function judgeBoard() {
+    isJudging = false;
+    return;
+
+    for (let y = 0; y < boardHeight; ++y) {
+        for (let x = 0; x < boardWidth; ++x) {
         }
     }
 }
